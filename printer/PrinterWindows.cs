2942658b58a34a930c9a232562fb328f7de855a8
Qiraht <48444430+Qiraht@users.noreply.github.com>
2019-05-14 12:11:09 +0700

    Commit Pertama

---
+++ printer/PrinterWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace printer
+{
+    public class PrinterWindows
+    {
+      
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Anda sedang Menggunakan Printer");
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Printer sedang mencetak");
+        }
+    }
+}
---
+++ printer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows pr;
+
+            Console.WriteLine("Pilih Printer :");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. Laserjet \n");
+
+            Console.WriteLine("Nomor Printer [1..3]: ");
+            int nomor = Convert.ToInt32(Console.ReadLine());
+
+            if (nomor == 1)
+                pr = new Epson();
+            else if (nomor == 2)
+                pr = new Canon();
+            else
+                pr = new Laserjet();
+
+            pr.Show();
+            pr.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
